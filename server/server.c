7fc12eea49fc2ac6f45e0c29d12290728520ea97
Bruno De Vivo <devivobruno01@gmail.com>
2025-04-17 22:19:56 +0200

    multithread handling added in server.c
    server file now handles multithreading, through the clientHandlerThread method it creates new threads thanks to pthread.h POSIX standard library

--- server/server.c
+++ server/server.c
@@ -1,10 +1,13 @@
 #include "server.h"
 #include "connection.h"
+#include "logger.h"
+#include <errno.h>
 
 int serverFileDescriptor;
+volatile int serverRunning = 1;
 
 int initializeServer(){
-    printf("Server Initialization on Port: 8080\n");
+    LOG_INFO("Server Initialization on Port: 8080");
     serverFileDescriptor = createSocket(8080);
 
     if(serverFileDescriptor < 0){
@@ -15,21 +18,65 @@ int initializeServer(){
 }
 
 void runServer(){
-    while(1){
-        printf("Waiting for Connection...\n");
-        int clientFileDescriptor = acceptConnection(serverFileDescriptor);
+    pthread_attr_t threadAttr;
+    int attr_result;
+    
+    if ((attr_result = pthread_attr_init(&threadAttr)) != 0) {
+        LOG_ERROR("Failed to initialize thread attributes: %s", strerror(attr_result));
+        return;
+    }
+    
+    if ((attr_result = pthread_attr_setdetachstate(&threadAttr, PTHREAD_CREATE_DETACHED)) != 0) {
+        LOG_ERROR("Failed to set detached thread state: %s", strerror(attr_result));
+        pthread_attr_destroy(&threadAttr);
+        return;
+    }
+    
+    while(serverRunning){
+        LOG_INFO("Waiting for Connection...");
+        
+        struct sockaddr_in clientAddress;
+        int clientFileDescriptor = acceptConnection(serverFileDescriptor, &clientAddress);
 
         if(clientFileDescriptor < 0){
-            fprintf(stderr, "Connection Failure!");
+            LOG_ERROR("Connection Failure!");
             continue;
         }
 
-        printf("Connection Successful!");
-        handleClient(clientFileDescriptor);
-        close(clientFileDescriptor);
+        LOG_CLIENT_INFO(&clientAddress, "Connection Successful");
+        
+        client_data_t* clientData = malloc(sizeof(client_data_t));
+        if (!clientData) {
+            LOG_ERROR("Failed to allocate memory for client data: %s", strerror(errno));
+            close(clientFileDescriptor);
+            continue;
+        }
+        
+        clientData->clientFileDescriptor = clientFileDescriptor;
+        clientData->clientAddress = clientAddress;
+        
+        pthread_t threadId;
+        int result = pthread_create(&threadId, &threadAttr, clientHandlerThread, (void*)clientData);
+        
+        if (result != 0) {
+            LOG_CLIENT_ERROR(&clientAddress, "Failed to create thread: %s", strerror(result));
+            close(clientFileDescriptor);
+            free(clientData);
+        } else {
+            LOG_CLIENT_DEBUG(&clientAddress, "Created new thread with ID %lu for client %s:%d", 
+                           (unsigned long)threadId,
+                           inet_ntoa(clientAddress.sin_addr), 
+                           ntohs(clientAddress.sin_port));
+        }
     }
+    
+    pthread_attr_destroy(&threadAttr);
 }
 
 void shutdownServer(){
+    serverRunning = 0;
     close(serverFileDescriptor);
+    
+    LOG_INFO("Server socket closed, waiting for threads to complete");
+    sleep(1);
 }
--- server/server.h
+++ server/server.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <string.h>
 
 int initializeServer();
 
